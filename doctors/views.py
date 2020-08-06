6c4846a1a9385ce42f7d3f626403f35ffdda7b78
boltyu <lightingyu@gmail.com>
2020-08-06 21:47:29 +0800

    2020080603

--- doctors/views.py
+++ doctors/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http.response import HttpResponse, JsonResponse
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
+from .models import Doctors
 # Create your views here.
 @login_required
 def Index(request):
@@ -13,9 +14,11 @@ def Login(request):
     username = request.POST['username']
     password = request.POST['password']
     user = authenticate(request,username=username,password=password)
+    
     if user is not None:
+        doctor = Doctors.objects.get(pk=user.pk)
         login(request,user)
-        request.session['doctor'] = user.doctorname
+        request.session['doctor'] = doctor.doctorname
         
     else:
         re['result'] = 500
--- patientcloud/settings.py
+++ patientcloud/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '03aqyw2nvd#_rkr*ce+9%=_l!*kbyh!k%aj&o(2saq=mmq0&47'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.1.132"]
 
 
 # Application definition
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -81,7 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
-            'read_default_file': '~/Works/patientcloud/db.cnf',
+            'read_default_file': 'db.cnf',
         },       
     }
 }
